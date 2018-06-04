b06774f275a1d7d02a9c538de52ab897100c1de3
Matthew Zipkin <pinheadmz@gmail.com>
2018-06-04 23:58:06 +0000

    init

---
+++ bpi.py
@@ -0,0 +1,164 @@
+import requests
+import curses
+import pyqrcode
+import atexit
+import time
+import os, sys
+import json
+
+### default endpoints, not using any API key
+# testnet
+#nodeurl = "http://127.0.0.1:18332/"
+#wallurl = "http://127.0.0.1:18334/wallet/primary/"
+# main
+nodeurl = "http://127.0.0.1:8332/"
+wallurl = "http://127.0.0.1:8334/wallet/primary/"
+
+### set up some functions to get data from bcoin
+def getInfo():
+	return requests.get(nodeurl).json()
+
+def getAddress():
+	params = {"account":"default"}
+	return requests.post(wallurl + 'address', json=params).json()
+
+def getBalance():
+	return requests.get(wallurl + 'balance?account=default').json()
+
+### read the JSON files from disk, store the last 20 in memory, and remove the files
+BLOCKS_DIR = '/home/pi/blocks/'
+BLOCKS = {}
+def readFiles(dict, dir, log):
+	files = [files for (dirpath, dirnames, files) in os.walk(dir)][0]
+	for index in files:
+		if log:
+			sys.stdout.write("\rHeight: " + str(index))
+		try:
+			with open (dir + index) as file:
+				dict[int(index)] = json.load(file)
+		except:
+			pass
+		sortedDictKeys = sorted(dict)
+		if len(sortedDictKeys) > 20:
+			del dict[sortedDictKeys[0]]
+
+### display address and QR code
+def displayAddr():
+	addr = getAddress()['address']
+	code = pyqrcode.create(addr, 'M', version=3).terminal()
+	
+	# switch back to terminal
+	endCurses()
+	# display QR code
+	os.system('clear')
+	print addr
+	print
+	print code
+	print
+	raw_input("Press Enter to continue...")
+	# switch back to our curses UI
+	startCurses()
+	
+### print the text info display
+def printInfo(info, balance):
+	# pull the relevant bits from the data
+	progress = info['chain']['progress']
+	latestHeight = info['chain']['height']
+	latestHash = info['chain']['tip']
+	confbal = balance['confirmed']
+	unconfbal = balance['unconfirmed']
+	# print it to the screen
+	stdscr.erase()
+	stdscr.addstr(0, 0, "Progress: " + str(int(progress*100000000)/1000000.0) + "%")
+	stdscr.addstr(1, 0, "Height: " + str(latestHeight))
+	stdscr.addstr(2, 0, "Hash: " + str(latestHash))
+	stdscr.addstr(3, 0, "Confirmed balance: " + str(confbal))
+	stdscr.addstr(4, 0, "Unconfirmed balance: " + str(unconfbal))
+	
+	#stdscr.addstr(5, 0, str(time.time()))
+
+	drawBlockchain()
+
+	# print menu on bottom
+	menu = "[Q]uit   [D]eposit"
+	stdscr.addstr(MAXYX[0]-1, 0, menu)
+	stdscr.refresh()
+
+### draw the recent blockchain
+WINDOW = 30 * 60 # total seconds across width of screen
+def drawBlockchain():
+	secondsPerCol = WINDOW/MAXYX[1]
+	stdscr.addstr(6, 0, "[" + "-" * (MAXYX[1]-2) + "]")
+	now = int(time.time())
+	
+	for index, block in BLOCKS.items():
+		secondsAgo = now - block['time']
+		
+		if secondsAgo < WINDOW:
+			col = MAXYX[1] - (secondsAgo / secondsPerCol) - 8
+			if col > 0:
+				stdscr.addstr(6, col, "|")
+				stdscr.addstr(7, col, "#" + str(index))
+				stdscr.addstr(8, col, "Hash:")
+				for i in range(8):
+					stdscr.addstr(9+i, col+1, block['hash'][i*8:i*8+8])
+				stdscr.addstr(17, col, "TXs:")
+				stdscr.addstr(18, col+1, str(block['totalTX']))
+				stdscr.addstr(19, col, "Age:")
+				stdscr.addstr(20, col+1, str(secondsAgo/60) + ":" + str(secondsAgo%60).zfill(2))
+
+### start the curses text-based terminal interface
+REFRESH = 1 # refresh rate in seconds
+MAXYX = None
+stdscr = None
+def startCurses():
+	global stdscr, MAXYX
+	stdscr = curses.initscr()
+	curses.noecho()
+	curses.cbreak()
+	curses.halfdelay(REFRESH * 10) # blocking value is x 0.1 seconds
+	MAXYX = stdscr.getmaxyx() # store window dimensions
+startCurses()
+def endCurses():
+	curses.nocbreak()
+	curses.echo()
+	curses.endwin()
+
+### automatically cleanup curses settings on exit
+def cleanup():
+	time.sleep(10)
+	endCurses()
+	os.system('clear')
+	print "bye!"
+atexit.register(cleanup)
+
+### stash cursor in the bottom right corner
+def hideCursor():
+	stdscr.addstr(MAXYX[0]-1, MAXYX[1]-1, "")
+
+### check for keyboard input -- also serves as the pause between REFRESH cycles
+def checkKeyIn():
+	keyNum = stdscr.getch()
+	if keyNum == -1:
+		return False
+	else:
+		key = chr(keyNum)
+	if key in ("q", "Q"):
+		sys.exit()
+	if key in ("d", "D"):
+		displayAddr()
+
+### the main loop!
+os.system('clear')
+print "Syncing with bcoin..."
+readFiles(BLOCKS, BLOCKS_DIR, True)
+while True:
+	# read block headers from files
+	readFiles(BLOCKS, BLOCKS_DIR, False)
+	# get data from servers
+	info = getInfo()
+	balance = getBalance()
+	# draw!
+	printInfo(info, balance)
+	hideCursor()
+	checkKeyIn()
---
+++ spv_clock.js
@@ -0,0 +1,74 @@
+const fs = require('fs');
+
+// create the a directory for block headers if it doesn't exist already
+const blocksDir = '/home/pi/blocks/';
+if (!fs.existsSync(blocksDir)){
+    fs.mkdirSync(blocksDir);
+}
+
+// Write a JSON object to disk
+function writeFile(index, element, dir){
+	fs.writeFile(dir + index, JSON.stringify(element), function(err){
+		if(!err){
+			// delete files older than 20 blocks if it exists
+			try {
+				fs.unlinkSync(dir + (index-20));
+			} catch(err) {
+				console.log(err);
+			}
+		}
+	});
+}
+
+// Get the SPV node object from the globally-installed bcoin package
+const bcoin = require('bcoin');
+
+// Configure the node for mainnet, write logs, use the database on disk, etc
+const node = new bcoin.SPVNode({
+  network: 'main',
+  config: true,
+  argv: true,
+  env: true,
+  logFile: true,
+  logConsole: true,
+  logLevel: 'debug',
+  db: 'leveldb',
+  memory: false,
+  persistent: true,
+  workers: true,
+  listen: true,
+  loader: require
+});
+
+// Add wallet
+node.use(bcoin.wallet.plugin);
+
+(async () => {
+
+  // Validate the prefix directory (probably ~/.bcoin)
+  await node.ensure();
+  // Open the node and all its child objects, wait for the database to load
+  await node.open();
+  // Connect to the network
+  await node.connect();
+  
+  // write new block details to file
+  node.on('block', async (block) => {
+  	// most of the block's details are returned by the 'block' event but we need to get its height from the blockchain database
+  	headers = await node.chain.getEntryByHash(block.hash('hex'));
+  	blockHeight = headers.height;
+
+	// simplify the block data structure to a string
+  	blockJSON = block.toJSON();
+
+	// index it by height (orphaned blocks will therefore be replaced by new block at same height)
+  	writeFile(blockHeight, blockJSON, blocksDir);
+  });
+  
+  // Start the blockchain sync
+  node.startSync();
+  
+})().catch((err) => {
+  console.error(err.stack);
+  process.exit(1);
+});
